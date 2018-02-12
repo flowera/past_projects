0cb6facfc3af0e8a119a37a27fb4c22f9e2ede2a
Rubing Guo <rubing1223@gmail.com>
2018-02-12 17:57:52 -0600

    minor change

--- pcc/ch8_functions/print_models.py
+++
@@ -1,7 +0,0 @@
-import printing_funcs as pf
-a = ['Tony', 'Heath']
-b = []
-#Python 3 Printing
-#print ("this is a number: %d." % (8))
-#print ("this is another number: {}".format(888))
-pf.print_models(a,b)
